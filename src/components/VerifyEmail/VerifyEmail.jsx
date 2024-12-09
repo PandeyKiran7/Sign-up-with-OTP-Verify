f4c6b9a890bc8c37080bc1e5b00dc2656e160a6a
Your Name <kiranmmm138@gmail.com>
2024-12-09 18:54:25 +0545

    ....

--- src/components/VerifyEmail/VerifyEmail.jsx
+++ src/components/VerifyEmail/VerifyEmail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./verifyEmail.css";
+import "../../Style/verifyEmail.css";
 
 
 const VerifyEmail = () => {
