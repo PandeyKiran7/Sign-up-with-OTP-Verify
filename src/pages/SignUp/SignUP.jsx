eb56cae94adeae2774f2d959b991277918f4cb4a
Your Name <kiranmmm138@gmail.com>
2024-12-09 17:44:40 +0545

    small change

--- src/pages/SignUp/SignUP.jsx
+++ src/pages/SignUp/SignUP.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./SignUp.css"
+import './SignUp.css';
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 
